7ae89a5ff205e5a0a060e026dba4f4773b71c23f
Petr Kalambet <peter@technogeek.io>
2016-02-08 08:39:54 +0300

    Update for IBM Containers Volume support in IBM Bluemix

--- 3.0/docker-entrypoint.sh
+++ 3.0/docker-entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'redis-server' ]; then
-	chown -R redis .
-	exec gosu redis "$@"
+if [ ! -e "/root/.db_init" ]
+then
+	adduser redis root
+	chmod 775 /mnt/redisdata
+
+	su -c "mkdir -p /mnt/redisdata" redis
+	ln -sf /mnt/redisdata /data
+	chown -h redis:redis /data
+
+	deluser redis root
+	chmod 755 /mnt/redisdata
+
+	touch /root/.db_init
 fi
 
-exec "$@"
+cd /data
+exec $@
